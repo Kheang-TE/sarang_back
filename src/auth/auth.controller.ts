8d5b723858268e959217e88813140e9411561f9e
Kheang-TE <kheang.te07@gmail.com>
2024-09-27 22:55:37 +0200

    feat: hash/compare password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+export type AuthBody = {
+    email: string;
+    password: string;
+}
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('login')
+    async login(@Body() authBody: AuthBody) {
+        return this.authService.login({authBody});
+    }
+
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { AuthBody } from './auth.controller';
+import { PrismaService } from 'src/utils/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private readonly prisma: PrismaService){}
+
+    async login({authBody} : {authBody: AuthBody}) {
+
+        const {email, password} = authBody;
+
+        const hashPassword = await this.hashPassword({password});
+
+        const existingUser = await this.prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
+
+        if (!existingUser) {
+            throw new Error('User not found');
+        }
+
+        const isPasswordValid = await this.isPasswordValid({password, hashedPassword: existingUser.password});
+
+        if(!isPasswordValid) {
+            throw new Error('Invalid password');
+        }
+
+        return existingUser.id
+    }
+
+    private async hashPassword( {password} : {password: string}) {
+        const hasedPassword = await bcrypt.hash(password, 10);
+        return hasedPassword;
+    }
+
+    private async isPasswordValid( {password, hashedPassword} : {password: string; hashedPassword: string}) {
+        const isPasswordValid = await bcrypt.compare(password, hashedPassword);
+        return isPasswordValid;
+    }
+
+}
