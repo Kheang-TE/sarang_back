c5115e92e1d336bb186f0dc19166672a51ab804a
Kheang-TE <kheang.te07@gmail.com>
2024-09-27 21:19:20 +0200

    feat : user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { MovieModule } from './movie/movie.module';
 import { HttpModule } from '@nestjs/axios';
 import { TvModule } from './tv/tv.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [HttpModule, MovieModule, TvModule],
+  imports: [HttpModule, MovieModule, TvModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/utils/prisma.service';
+
+@Injectable()
+export class UserService {
+
+    constructor(private readonly prisma:PrismaService) {}
+
+    async getUsers() {
+        const users = await this.prisma.user.findMany({
+            select: {
+                id: true,
+                username: true,
+                email: true
+            }
+        });
+        return users;
+    }
+
+    async getUserById( userId: string) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: userId
+            },
+            select: {
+                id: true,
+                username: true,
+                email: true
+            }
+        });
+        return user;
+    }
+}
